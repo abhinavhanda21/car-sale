b25fa83877ceec693afc2160d79de18a15ec5d70
abhihanda23 <abhihanda23@gmail.com>
2018-04-28 13:02:30 +0530

    second commit

--- nacionais.php
+++ nacionais.php
@@ -1,7 +1,7 @@
 
 <div class="well">
 	<div class="container">
-		<h1>Nationals</h1>
+		<h1>Our pick</h1>
 		<p>The 8 most economical cars of 2017</p>
 	</div>
 </div> <!-- fim do well -->
@@ -13,36 +13,54 @@
                     <img src="imgs/nacionais/ciaz.jpg" alt="peugeot" class="img-responsive">
 			<h3>Maruti Suzuki Ciaz SHVS</h3>
 			<p>Prices of the current-gen Maruti Ciaz start from Rs 7.83 lakh and goes up to 10.63 lakh for the petrol version while the diesel-powered sedan is priced between Rs 9.49 lakh and Rs 11.62 lakh. </p>
+							<a href="Ciaz" class="btn btn-danger">
+				<i class="fa fa-search"></i> Details
+			</a>
 		</div>
 		<div class="col-md-3 col-sm-6 col-xs-12 text-center">
                     <img src="imgs/nacionais/cel.jpg" alt="nissanversa" class="img-responsive">
 			<h3>Maruti Suzuki Celerio </h3>
 			<p>Maruti Suzuki updated its Celerio in October, 2017, with subtle changes to the hatchback’s exterior and interior. At a starting price of Rs 4.15 lakh (ex-showroom Delhi), the Celerio facelift is available in 12 variants - 10 petrol and 2 CNG.</p>
+							<a href="Celerio" class="btn btn-danger">
+				<i class="fa fa-search"></i> Details
+			</a>
 		</div>
 		<div class="col-md-3 col-sm-6 col-xs-12 text-center">
                     <img src="imgs/nacionais/bal.jpg" alt="voyage" class="img-responsive">
 			<h3>Maruti Baleno Diesel</h3>
 			<p>Maruti Baleno is available at a starting price of Rs 5.35 lakh for the petrol and 6.51 lakh for the diesel variant. The top variants are priced at Rs 8.40 lakh and 8.49 lakh respectively. It is available in four variants: Sigma, Delta, Zeta and Alpha.</p>
+							<a href="Baleno" class="btn btn-danger">
+				<i class="fa fa-search"></i> Details
+			</a>
 		</div>
 
 	
 		<div class="col-md-3 col-sm-6 col-xs-12 text-center">
-                    <img src="imgs/nacionais/jazz.jpg" alt="gol" class="img-responsive">
-			<h3>Honda Jazz Diesel</h3>
-			<p>The Honda Jazz is available in five variants:  E, S, SV, V, and VX. Apart from this, there is a CVT (continuously variable transmission) auto on offer on the S CVT and V CVT variants, albeit exclusively with the petrol engine. Prices start at Rs 5.99 lakh and go up to Rs 9.31 lakh</p>
+                    <img src="imgs/dezire.jpg" alt="gol" class="img-responsive">
+			<h3>Maruti Swift Dzire</h3>
+			<p>Maruti Swift Dzire prices start at Rs. 5.56 lakh for petrol and goes upto Rs. 8.43 lakh. The prices of Swift Dzire diesel variants start at Rs. 6.56 lakh and the top-end diesel is priced at Rs. 9.43 lakh. Maruti Swift Dzire is available in 14 variants and 6 colours.</p>
+							<a href="Swift-dezire" class="btn btn-danger">
+				<i class="fa fa-search"></i> Details
+			</a>
 		</div>
 		</div>
         <h2><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> Best in Budget</h2>
         <div class="row">
 		<div class="col-md-3 col-sm-6 col-xs-12 text-center">
-                    <img src="imgs/nacionais/tia.jpg" alt="fiatuno" class="img-responsive">
-			<h3>Tata Tiago</h3>
-			<p> The top-spec XZ variant can also be had without alloy wheels. There’s also a Wizz edition with an accessory pack available with both petrol and diesel engine options. Prices start at Rs 3.35 lakh and go up to Rs 5.99 lakh</p>
+                    <img src="imgs/swift.jpg" alt="fiatuno" class="img-responsive">
+			<h3>Maruti Swift</h3>
+			<p> Maruti Swift prices start at Rs. 4.99 lakh for petrol and goes upto Rs. 7.29 lakh. The prices of Swift diesel variants start at Rs. 5.99 lakh and the top-end diesel is priced at Rs. 8.29 lakh. Maruti Swift is available in 12 variants and 6 colours.</p>
+							<a href="Swift" class="btn btn-danger">
+				<i class="fa fa-search"></i> Details
+			</a>
 		</div>
 		<div class="col-md-3 col-sm-6 col-xs-12 text-center">
-                    <img src="imgs/nacionais/kwid.jpg" alt="fiatmobi" class="img-responsive">
-			<h3>Renault Kwid</h3>
-			<p>The Renault Kwid prices range between Rs 2.61 lakh-3.83 lakh for the 800cc versions and Rs 3.58-4.94 lakh for the 1.0-litre versions (all prices ex-showroom New Delhi). The Renault Kwid Climber, which is offered only with the larger 1-litre engine, is priced at Rs 4.29 lakh for the manual version and Rs 4.59 lakh for the AMT-equipped model.</p>
+                    <img src="imgs/i20.jpg" alt="fiatmobi" class="img-responsive">
+			<h3>Hyundai Elite i20</h3>
+			<p>Hyundai Elite i20 prices start at Rs. 5.35 lakh for petrol and goes upto Rs. 7.91 lakh. The prices of Elite i20 diesel variants start at Rs. 6.73 lakh and the top-end diesel is priced at Rs. 9.16 lakh. Hyundai Elite i20 is available in 12 variants and 9 colours.</p>
+							<a href="i20" class="btn btn-danger">
+				<i class="fa fa-search"></i> Details
+			</a>
 		</div>
 		</div>
 		<div class="row">
@@ -50,6 +68,9 @@
                     <img src="imgs/nacionais/alto.jpg" alt="fiatpalio" class="img-responsive">
 			<h3>Maruti Suzuki Alto 800</h3>
 			<p>The Alto 800 received a facelift in 2017 and now sports a revised front fascia with a slimmer front grille and headlamp, and a larger air intake. The Alto 800 is Maruti Suzuki’s entry-level offering in the Indian four-wheeler market. The Alto 800 is priced between Rs 2.51 lakh and Rs 3.78 lakh and is one of the most popular hatchbacks in its segment.</p>
+							<a href="Alto-800" class="btn btn-danger">
+				<i class="fa fa-search"></i> Details
+			</a>
 		</div>
 	</div>
 </div> <!-- fim do container -->
